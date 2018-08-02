311bbeb9c9840174486a40b812565f7951b2e10f
pulcino <alfredo.pulcino@afp.com>
2018-08-02 17:01:53 +0200

    ajouter API health

--- application.js
+++ application.js
@@ -4,4 +4,7 @@ const application = express();
 const apiEventsRoutes = require('./APIEvents');
 application.use('/api/events', apiEventsRoutes);
 
+const apiHealthRoutes = require('./APIHealth');
+application.use('/health', apiHealthRoutes);
+
 module.exports = application;
